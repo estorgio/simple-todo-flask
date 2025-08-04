cadd52c38fc1dc717b8f4d6a424e89b75312c39f
Fortunato Estorgio <fortunato.estorgio@gmail.com>
2025-08-04 14:44:24 +0800

    Suppress errors when '/tmp/app/storage' already exists

--- extensions/cloud_demo.py
+++ extensions/cloud_demo.py
@@ -4,7 +4,7 @@ import shutil
 
 def deploy_database():
     if os.getenv('GAE_ENV') == 'standard' or os.getenv('RENDER') == 'true':
-        os.makedirs('/tmp/app/storage')
+        os.makedirs('/tmp/app/storage', exist_ok=True)
 
         with open('/tmp/app/storage/Readme.txt', 'w') as file:
             file.write('This is a storage directory for app.')
