60b157fb293518589977009d5369224f38f77f7f
Fortunato Estorgio <fortunato.estorgio@gmail.com>
2025-08-05 18:11:36 +0800

    Use 'request.remote_addr' instead to get client IP address

--- extensions/proxy.py
+++ extensions/proxy.py
@@ -15,7 +15,5 @@ def init_app(app: Flask) -> None:
 
     @app.context_processor
     def client_ip_address() -> dict:
-        ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-        if ip and ',' in ip:
-            ip = ip.split(',')[0].strip()
+        ip = request.remote_addr
         return dict(client_ip=ip)
